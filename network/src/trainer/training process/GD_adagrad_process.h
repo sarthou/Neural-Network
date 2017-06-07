9caa9d8623dff101b99704fb69d456ca4689d2f7
guillaume <guillaum324bis@hotmail.fr>
2017-06-07 16:04:45 +0200

    add adagrad process

---
+++ network/src/trainer/training process/GD_adagrad_process.h
@@ -0,0 +1,47 @@
+//============================================================================
+// Name        : GD_adagrad_process.h
+// Authors     : Guillaume Sarthou
+// EMail       : open.pode@gmail.com
+// Date		   : 7 jun. 2017
+// Version     : V1.3
+// Copyright   : This file is part of SNN_network project which is released under
+//               MIT license.
+//============================================================================
+#pragma once
+
+#ifndef GD_ADAGRAD_PROCESS_H
+#define GD_ADAGRAD_PROCESS_H
+
+#include "Trainig_process.h"
+
+namespace SNN_network
+{
+
+	class GD_adagrad_process : public Trainig_process
+	{
+	public:
+		GD_adagrad_process(Perceptron* p_perceptron, double p_step);
+		~GD_adagrad_process();
+
+		void init();
+		void set_error(double T);
+		void propagate(vector<Trainig_process*> process, bool out = false);
+		void compute();
+
+		void add(double value);
+
+	private:
+		double m_step;
+		double m_bia_gradient;
+		vector<double> m_w_gradient;
+
+		double ss_gradient;
+
+		double m_error;
+		double m_gradient;
+		double m_delta;
+		double m_delta_1;
+	};
+} // SNN_network
+
+#endif // !GD_ADAGRAD_PROCESS_H
